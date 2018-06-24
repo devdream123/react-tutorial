cf7c1ff813e93173478b69518a0b79f83bd6dc68
Dannie <devdream123@github.com>
2018-06-25 09:50:02 +1000

    Tutorials

---
+++ src/components/Order.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { formatPrice } from "../helpers";
+import { link } from "fs";
+class Order extends React.Component {
+	render() {
+		const orderIds = Object.keys(this.props.order);
+		const total = orderIds.reduce((prevTotal, key) => {
+			const fish = this.props.fishes[key];
+			const count = this.props.order[key];
+			const isAvailable = fish && fish.status === "available";
+			if (isAvailable) {
+				return prevTotal + count * fish.price;
+			}
+			return prevTotal;
+		}, 0);
+		return (
+			<div className="order-wrap">
+				<h2>Order</h2>
+				<ul>{orderIds.map(key => <li>{key}</li>)}</ul>
+				<div className="total" />
+				Total:
+				<strong>{formatPrice(total)}</strong>
+			</div>
+		);
+	}
+}
+
+export default Order;
